5c55a8a645394a7a717f6afa322b363584c30920
Catherine Walters <caw8616@g.rit.edu>
2021-02-25 11:08:32 -0500

    Push To git

---
+++ data.js
@@ -0,0 +1,303 @@
+var data = new Object();
+
+data['leagues']=['Choose a division...', 'AFC North', 'AFC South', 'AFC West', 'AFC East', 'NFC North', 'NFC South', 'NFC West', 'NFC East'];
+
+//Divisions
+
+data['AFC North']=['Choose a team...','Ravens','Bengals','Browns','Steelers'];
+
+data['AFC South']=['Choose a team...','Texans','Colts','Jaguars','Titans'];
+
+data['AFC West']=['Choose a team...','Broncos', 'Chiefs', 'Raiders','Chargers'];
+
+data['AFC East']=['Choose a team...','Bills', 'Dolphins', 'Patriots','Jets'];
+
+data['NFC North']=['Choose a team...','Bears','Lions', 'Packers','Vikings'];
+
+data['NFC South']=['Choose a team...','Falcons','Panthers','Saints','Buccaneers'];
+
+data['NFC West']=['Choose a team...','Cardinals','Rams','49ers','Seahawks'];
+
+data['NFC East']=['Choose a team...','Cowboys', 'Giants', 'Eagles', 'Redskins'];
+
+//AFC North
+
+data['Ravens']=['Choose a year...','Ravens 1999-Present','Ravens 1996-1998'];
+
+data['Bengals']=['Choose a year...', 'Bengals 2004-Present', 'Bengals 1997-2003','Bengals 1981-1996','Bengals 1970-1980','Bengals 1968-1969' ];
+
+data['Browns']=['Choose a year...','Browns 2006-Present','Browns 1992-2005', 'Browns 1986-1991','Browns 1970-1985','Browns 1959-1969', 'Browns 1948-1958'];
+
+data['Steelers']=['Choose a year...','Steelers 2002-Present','Steelers 1969-2001', 'Steelers 1962-1968','Steelers 1955-1961','Steelers 1945-1954'];
+
+//AFC South
+
+data['Texans']=['Choose a year...','Texans 2002-Present'];
+
+data['Colts']=['Choose a year...','Colts 2002-Present', 'Colts 1984-2001','Colts 1961-1978', 'Colts 1953-1960'];
+
+data['Jaguars']=['Choose a year...','Jaguars 2013-Present','Jaguars 1995-2012'];
+
+data['Titans']=['Choose a year...','Titans 2013-Present','Oilers 1980-1990','Oilers 1972-1979','Oilers 1969-1971','Oilers 1961-1968','Oilers 1960-1961'];
+
+//AFC West
+
+data['Broncos']=['Choose a year...','Broncos 1997-Present','Broncos 1970-1992','Broncos 1962-1969','Broncos 1960-1961'];
+
+data['Chiefs']=['Choose a year...','Chiefs 1972-Present','Chiefs 1956-1971','Chiefs 1952-1955'];
+
+
+data['Raiders']=['Choose a year...','Raiders 1964-Present','Raiders 1963-1963','Raiders 1960-1962' ];
+
+data['Chargers']=['Choose a year...','Chargers 2007-Present','Chargers 2002-2006','Chargers 1988-2001','Chargers 1974-1987','Chargers 1961-1973','Chargers 1960-1960'];
+
+//AFC East
+
+data['Bills']=['Choose a year...','Bills 1974-Present','Bills 1970-1973','Bills 1965-1969','Bills 1962-1964','Bills 1960-1961'];
+
+data['Dolphins']=['Choose a year...','Dolphins 2013-Present', 'Dolphins 1997-2012', 'Dolphins 1989-1996','Dolphins 1974-1989','Dolphins 1966-1973'];
+
+data['Patriots']=['Choose a year...','Patriots 2000-Present', 'Patriots 1993-1999', 'Patriots 1971-1992', 'Patriots 1965-1970', 'Patriots 1961-1964', 'Patriots 1960-1960'];
+
+data['Jets']=['Choose a year...','Jets 1998-Present', 'Jets 1978-1997', 'Jets 1967-1977', 'Jets 1964-1966', 'Jets 1963-1963'];
+
+//NFC North
+
+data['Bears']=['Choose a year...','Bears 1974-Present','Bears 1962-1972','Bears 1954-1973','Bears 1940-1953'];
+
+data['Lions']=['Choose a year...','Lions 2009-Present','Lions 2003-2008','Lions 1970-2002','Lions 1961-1969','Lions 1952-1960'];
+
+data['Packers']=['Choose a year...','Packers 1980-Present','Packers 1961-1979','Packers 1956-1961','Packers 1951-1955'];
+
+data['Vikings']=['Choose a year...','Vikings 2013-Present','Vikings 1966-2012','Vikings 1961-1965'];
+
+//NFC South
+
+data['Falcons']=['Choose a year...','Falcons 2003-Present','Falcons 1990-2002','Falcons 1966-1989'];
+
+data['Panthers']=['Choose a year...','Panthers 2012-Present','Panthers 1995-2011'];
+
+data['Saints']=['Choose a year...','Saints 2000-Present','Saints 1967-1999'];
+
+data['Buccaneers']=['Choose a year...','Buccaneers 1997-Present','Buccaneers 1976-1996'];
+
+//NFC West
+
+data['Cardinals']=['Choose a year...','Cardinals 2005-Present','Cardinals 1960-2004','Cardinals 1947-1959','Cardinals 1920-1946'];
+
+data['Rams']=['Choose a year...','Rams 2000-Present','Rams 1995-1999','Rams 1981-1994','Rams 1973-1980','Rams 1950-1972','Rams 1940-1949'];
+
+data['49ers']=['Choose a year...','49ers 2009-Present','49ers 1996-2008','49ers 1968-1995','49ers 1946-1967'];
+data['Seahawks']=['Choose a year...','Seahawks 2012-Present','Seahawks 2002-2011','Seahawks 1976-2001'];
+
+//NFC East
+
+data['Cowboys']=['Choose a year...','Cowboys 1964-Present','Cowboys 1960-1963'];
+data['Giants']=['Choose a year...','Giants 2000-Present','Giants 1976-1999','Giants 1975-1975','Giants 1961-1974','Giants 1956-1960','Giants 1950-1955','Giants 1945-1949'];
+data['Eagles']=['Choose a year...','Eagles 1996-Present','Eagles 1987-1995','Eagles 1969-1986','Eagles 1948-1968'];
+data['Redskins']=['Choose a year...','Redskins 1983-Present','Redskins 1982-1982','Redskins 1972-1981','Redskins 1970-1971','Redskins 1965-1969','Redskins 1960-1964','Redskins 1952-1959','Redskins 1937-1951','Redskins 1933-1936'];
+
+//AFC North
+//Ravens
+data['Ravens 1999-Present']=['End','AFC North','Baltimore Ravens','1999-Present','afc/north/ravens1999.png'];
+data['Ravens 1996-1998']=['End','AFC North','Baltimore Ravens','1996-1998','afc/north/ravens1996.jpg'];
+
+//Bengals
+data['Bengals 2004-Present']=['End','AFC North','Cincinnati Bengals','2004-Present','afc/north/bengals2004.gif'];
+data['Bengals 1997-2003']=['End','AFC North','Cincinnati Bengals','1997-2003','afc/north/bengals1997.gif'];
+data['Bengals 1981-1996']=['End','AFC North','Cincinnati Bengals','1981-1996','afc/north/bengals1981.jpg'];
+data['Bengals 1970-1980']=['End','AFC North','Cincinnati Bengals','1970-1980','afc/north/bengals1970.gif'];
+data['Bengals 1968-1969']=['End','AFC North','Cincinnati Bengals','1968-1969','afc/north/bengals1968.png'];
+
+//Browns
+data['Browns 2006-Present']=['End','AFC North','Cleveland Browns','2006-Present','afc/north/browns2006.gif'];
+data['Browns 1992-2005']=['End','AFC North','Cleveland Browns','1992-2005','afc/north/browns1992.gif'];
+data['Browns 1986-1991']=['End','AFC North','Cleveland Browns','1986-1991','afc/north/browns1986.gif'];
+data['Browns 1970-1985']=['End','AFC North','Cleveland Browns','1970-1985','afc/north/browns1970.gif'];
+data['Browns 1959-1969']=['End','AFC North','Cleveland Browns','1959-1969','afc/north/browns1959.gif'];
+data['Browns 1948-1958']=['End','AFC North','Cleveland Browns','1948-1958','afc/north/browns1948.png'];
+
+//Steelers
+data['Steelers 2002-Present']=['End','AFC North','Pittsburg Steelers','2002-Present','afc/north/steelers2002.png'];
+data['Steelers 1969-2001']=['End','AFC North','Pittsburg Steelers','1969-2001','afc/north/steelers1963.png'];
+data['Steelers 1962-1968']=['End','AFC North','Pittsburg Steelers','1962-1968','afc/north/steelers1962.png'];
+data['Steelers 1955-1961']=['End','AFC North','Pittsburg Steelers','1955-1961','afc/north/steelers1955.png'];
+data['Steelers 1945-1954']=['End','AFC North','Pittsburg Steelers','1945-1954','afc/north/steelers1945.png'];
+
+//AFC South
+//Texans
+data['Texans 2002-Present']=['End','AFC South','Houston Texans','2002-Present','afc/south/texans2002.png'];
+
+//Colts FIRST TWO BALTIMORE
+data['Colts 2002-Present']=['End','AFC South','Indianapolis Colts','2002-Present','afc/south/colts2002.jpg'];
+data['Colts 1984-2001']=['End','AFC South','Indianapolis Colts','1984-2001','afc/south/colts1984.png'];
+data['Colts 1961-1978']=['End','AFC South','Baltimore Colts','1961-1978','afc/south/colts1981.gif'];
+data['Colts 1953-1960']=['End','AFC South','Baltimore Colts','1953-1960','afc/south/colts1953.gif'];
+
+//Jaguars
+data['Jaguars 2013-Present']=['End','AFC South','Jacksonville Jaguars','2013-Present', 'afc/south/jaguars2013.png'];
+data['Jaguars 1995-2012']=['End','AFC South','Jacksonville Jaguars','1995-2012', 'afc/south/jaguars1995.png'];
+
+//Titans
+data['Titans 2013-Present']=['End','AFC South','Tennessee Titans','2013-Present', 'afc/south/titans1999.png'];
+data['Oilers 1980-1990']=['End','AFC South','Houston Oilers','1980-1990', 'afc/south/oil1980.jpg'];
+data['Oilers 1972-1979']=['End','AFC South','Houston Oilers','1972-1979', 'afc/south/oil1972.png'];
+data['Oilers 1969-1971']=['End','AFC South','Houston Oilers','1969-1971', 'afc/south/oil1969.gif'];
+data['Oilers 1961-1968']=['End','AFC South','Houston Oilers','1961-1968', 'afc/south/oil1961.gif'];
+data['Oilers 1960-1961']=['End','AFC South','Houston Oilers','1960-1961', 'afc/south/oil1960.png'];
+
+//AFC West
+//Broncos
+data['Broncos 1997-Present']=['End','AFC West','Denver Broncos','1997-Present', 'afc/west/broncos1997.png'];
+data['Broncos 1970-1992']=['End','AFC West','Denver Broncos','1970-1992', 'afc/west/broncos1970.gif'];
+data['Broncos 1962-1969']=['End','AFC West','Denver Broncos','1962-1969', 'afc/west/broncos1962.png'];
+data['Broncos 1960-1961']=['End','AFC West','Denver Broncos','1960-1961', 'afc/west/broncos1960.gif'];
+
+//Chiefs
+data['Chiefs 1972-Present']=['End','AFC West','Kansas City Chiefs','1972-Present', 'afc/west/chiefs1972.png'];
+data['Chiefs 1956-1971']=['End','AFC West','Kansas City Chiefs','1956-1971', 'afc/west/chiefs1956.gif'];
+data['Chiefs 1952-1955']=['End','AFC West','Kansas City Chiefs','1952-1955', 'afc/west/chiefs1952.jpeg'];
+
+//Raiders
+data['Raiders 1964-Present']=['End','AFC West','Oakland Raiders','1964-Present', 'afc/west/raiders1964.png'];
+data['Raiders 1963-1963']=['End','AFC West','Oakland Raiders','1963-1963', 'afc/west/raiders1963.jpeg'];
+data['Raiders 1960-1962']=['End','AFC West','Oakland Raiders','1960-1962', 'afc/west/raiders1960.jpeg'];
+
+//Chargers
+data['Chargers 2007-Present']=['End','AFC West','San Diego Chargers','2007-Present', 'afc/west/chargers2007.png'];
+data['Chargers 2002-2006']=['End','AFC West','San Diego Chargers','2002-2006', 'afc/west/chargers2002.gif'];
+data['Chargers 1988-2001']=['End','AFC West','San Diego Chargers','1988-2001', 'afc/west/chargers1988.gif'];
+data['Chargers 1974-1987']=['End','AFC West','San Diego Chargers','1974-1987', 'afc/west/chargers1974.gif'];
+data['Chargers 1961-1973']=['End','AFC West','San Diego Chargers','1961-1973', 'afc/west/chargers1961.gif'];
+data['Chargers 1960-1960']=['End','AFC West','San Diego Chargers','1960-1960', 'afc/west/chargers1960.gif'];
+
+//AFC East
+//Bills
+data['Bills 1974-Present']=['End','AFC East','Buffalo Bills','1974-Present', 'afc/east/bills1974.svg'];
+data['Bills 1970-1973']=['End','AFC East','Buffalo Bills','1970-1973', 'afc/east/bills1970.gif'];
+data['Bills 1965-1969']=['End','AFC East','Buffalo Bills','1965-1969', 'afc/east/bills1965.gif'];
+data['Bills 1962-1964']=['End','AFC East','Buffalo Bills','1962-1964', 'afc/east/bills1962.GIF'];
+data['Bills 1960-1961']=['End','AFC East','Buffalo Bills','1960-1961', 'afc/east/bills1960.gif'];
+
+//Dolphins
+data['Dolphins 2013-Present']=['End','AFC East','Miami Dolphins','2013-Present', 'afc/east/dolphins2013.jpg'];
+data['Dolphins 1997-2012']=['End','AFC East','Miami Dolphins','1997-2012', 'afc/east/dolphins1997.png'];
+data['Dolphins 1989-1996']=['End','AFC East','Miami Dolphins','1989-1996', 'afc/east/dolphins1989.png'];
+data['Dolphins 1974-1989']=['End','AFC East','Miami Dolphins','1974-1989', 'afc/east/dolphins1974.gif'];
+data['Dolphins 1966-1973']=['End','AFC East','Miami Dolphins','1966-1973', 'afc/east/dolphins1966.gif'];
+
+//Patriots
+data['Patriots 2000-Present']=['End','AFC East','New England Patriots','2000-Present', 'afc/east/patriots2000.gif'];
+data['Patriots 1993-1999']=['End','AFC East','New England Patriots','1993-1999', 'afc/east/patriots1993.png'];
+data['Patriots 1971-1992']=['End','AFC East','New England Patriots','1971-1992', 'afc/east/patriots1971.png'];
+data['Patriots 1965-1970']=['End','AFC East','New England Patriots','1965-1970', 'afc/east/patriots1965.gif'];
+data['Patriots 1961-1964']=['End','AFC East','New England Patriots','1961-1964', 'afc/east/patriots1961.gif'];
+data['Patriots 1960-1960']=['End','AFC East','New England Patriots','1960-1960', 'afc/east/patriots1960.png'];
+
+//Jets
+data['Jets 1998-Present']=['End','AFC East','New York Jets','1998-Present', 'afc/east/jets1998.gif'];
+data['Jets 1978-1997']=['End','AFC East','New York Jets','1978-1997', 'afc/east/jets1978.gif'];
+data['Jets 1967-1977']=['End','AFC East','New York Jets','1967-1977', 'afc/east/jets1967.gif'];
+data['Jets 1964-1966']=['End','AFC East','New York Jets','1964-1966', 'afc/east/jets1964.png'];
+data['Jets 1963-1963']=['End','AFC East','New York Jets','1963-1963', 'afc/east/jets1963.png'];
+
+
+//NFC North
+//Bears
+data['Bears 1974-Present']=['End','NFC North','Chicago Bears','1974-Present','nfc/north/bears1974.png'];
+data['Bears 1962-1972']=['End','NFC North','Chicago Bears','1962-1972','nfc/north/bears1962.png'];
+data['Bears 1954-1973']=['End','NFC North','Chicago Bears','1954-1973','nfc/north/bears1954.gif'];
+data['Bears 1940-1953']=['End','NFC North','Chicago Bears','1940-1953','nfc/north/bears1940.png'];
+
+//Lions
+data['Lions 2009-Present']=['End','NFC North','Detroit Lions','2009-Present','nfc/north/lions2009.jpg'];
+data['Lions 2003-2008']=['End','NFC North','Detroit Lions','2003-2008','nfc/north/lions2003.gif'];
+data['Lions 1970-2002']=['End','NFC North','Detroit Lions','1970-2002','nfc/north/lions1970.gif'];
+data['Lions 1961-1969']=['End','NFC North','Detroit Lions','1961-1969','nfc/north/lions1961.png'];
+data['Lions 1952-1960']=['End','NFC North','Detroit Lions','1952-1960','nfc/north/lions1952.png'];
+
+//Packers
+data['Packers 1980-Present']=['End','NFC North','Green Bay Packers','1980-Present','nfc/north/packers1980.png'];
+data['Packers 1961-1979']=['End','NFC North','Green Bay Packers','1961-1979','nfc/north/packers1961.png'];
+data['Packers 1956-1961']=['End','NFC North','Green Bay Packers','1956-1961','nfc/north/packers1956.png'];
+data['Packers 1951-1955']=['End','NFC North','Green Bay Packers','1951-1955','nfc/north/packers1951.gif'];
+
+//Vikings
+data['Vikings 2013-Present']=['End','NFC North','Minnesota Vikings','2013-Present','nfc/north/vikings2013.png'];
+data['Vikings 1966-2012']=['End','NFC North','Minnesota Vikings','1966-2012','nfc/north/vikings1966.jpg'];
+data['Vikings 1961-1965']=['End','NFC North','Minnesota Vikings','1961-1965','nfc/north/vikings1961.gif'];
+
+//NFC South
+//Falcons
+data['Falcons 2003-Present']=['End','NFC South','Atlanta Falcons','2003-Present','nfc/south/falcons2003.png'];
+data['Falcons 1990-2002']=['End','NFC South','Atlanta Falcons','1990-2002','nfc/south/falcons1990.gif'];
+data['Falcons 1966-1989']=['End','NFC South','Atlanta Falcons','1966-1989','nfc/south/falcons1966.gif'];
+
+//Panthers
+data['Panthers 2012-Present']=['End','NFC South','Carolina Panthers','2012-Present','nfc/south/panthers2012.png'];
+data['Panthers 1995-2011']=['End','NFC South','Carolina Panthers','1995-2011','nfc/south/panthers1995.png'];
+
+//Saints
+data['Saints 2000-Present']=['End','NFC South','New Orleans Saints','2000-Present','nfc/south/saints2000.png'];
+data['Saints 1967-1999']=['End','NFC South','New Orleans Saints','1967-1999','nfc/south/saints1967.png'];
+
+//Buccaneers
+data['Buccaneers 1997-Present']=['End','NFC South','Tampa Bay Buccaneers','1997-Present','nfc/south/buccaneers1997.png'];
+data['Buccaneers 1976-1996']=['End','NFC South','Tampa Bay Buccaneers','1976-1996','nfc/south/buccaneers1976.png'];
+
+//NFC West
+//Cardinals
+data['Cardinals 2005-Present']=['End','NFC West','Arizona Cardinals','2005-Present','nfc/west/cardinals2005.png'];
+data['Cardinals 1960-2004']=['End','NFC West','Arizona Cardinals','1960-2004','nfc/west/cardinals1960.gif'];
+data['Cardinals 1947-1959']=['End','NFC West','Arizona Cardinals','947-1959','nfc/west/cardinals1947.png'];
+data['Cardinals 1920-1946']=['End','NFC West','Arizona Cardinals','1920-1946','nfc/west/cardinals1920.png'];
+
+//Rams
+data['Rams 2000-Present']=['End','NFC West','St.Louis Rams','2000-Present','nfc/west/rams2000.png'];
+data['Rams 1995-1999']=['End','NFC West','St.Louis Rams','1995-1999','nfc/west/rams1995.gif'];
+data['Rams 1981-1994']=['End','NFC West','St.Louis Rams','1981-1994','nfc/west/rams1981.gif'];
+data['Rams 1973-1980']=['End','NFC West','St.Louis Rams','1973-1980','nfc/west/rams1973.png'];
+data['Rams 1950-1972']=['End','NFC West','St.Louis Rams','1950-1972','nfc/west/rams1950.jpeg'];
+data['Rams 1940-1949']=['End','NFC West','St.Louis Rams','1940-1949','nfc/west/rams1940.gif'];
+
+//49ers
+data['49ers 2009-Present']=['End','NFC West','San Francisco 49ers','2009-Present','nfc/west/49ers2009.gif'];
+data['49ers 1996-2008']=['End','NFC West','San Francisco 49ers','1996-2008','nfc/west/49ers1996.png'];
+data['49ers 1968-1995']=['End','NFC West','San Francisco 49ers','1968-1995','nfc/west/49ers1968.png'];
+data['49ers 1946-1967']=['End','NFC West','San Francisco 49ers','1946-1967','nfc/west/49ers1946.gif'];
+
+//Seahawks
+data['Seahawks 2012-Present']=['End','NFC West','Seattle Seahawks','2012-Present','nfc/west/seahawks2012.png'];
+data['Seahawks 2002-2011']=['End','NFC West','Seattle Seahawks','2002-2011','nfc/west/seahawks2002.png'];
+data['Seahawks 1976-2001']=['End','NFC West','Seattle Seahawks','1976-2001','nfc/west/seahawks1976.png'];
+
+//NFC East
+//Cowboys
+data['Cowboys 1964-Present']=['End','NFC East','Dallas Cowboys','1964-Present','nfc/east/cowboys1960.png'];
+data['Cowboys 1960-1963']=['End','NFC East','Dallas Cowboys','1960-1963','nfc/east/cowboys1964.jpg'];
+
+//Giants
+data['Giants 2000-Present']=['End','NFC East','New York Giants','2000-Present','nfc/east/giants2000.gif'];
+data['Giants 1976-1999']=['End','NFC East','New York Giants','1976-1999','nfc/east/giants1976.gif'];
+data['Giants 1975-1975']=['End','NFC East','New York Giants','1975-1975','nfc/east/giants1975.png'];
+data['Giants 1961-1974']=['End','NFC East','New York Giants','1961-1974','nfc/east/giants1961.gif'];
+data['Giants 1956-1960']=['End','NFC East','New York Giants','1956-1960','nfc/east/giants1956.png'];
+data['Giants 1950-1955']=['End','NFC East','New York Giants','1950-1955','nfc/east/giants1950.png'];
+data['Giants 1945-1949']=['End','NFC East','New York Giants','1945-1949','nfc/east/giants1945.gif'];
+
+//Eagles
+data['Eagles 1996-Present']=['End','NFC East','Philadelphia Eagles','1996-Present','nfc/east/eagles1996.png'];
+data['Eagles 1987-1995']=['End','NFC East','Philadelphia Eagles','1987-1995','nfc/east/eagles1987.gif'];
+data['Eagles 1969-1986']=['End','NFC East','Philadelphia Eagles','1969-1986','nfc/east/eagles1969.gif'];
+data['Eagles 1948-1968']=['End','NFC East','Philadelphia Eagles','1948-1968','nfc/east/eagles1948.png'];
+
+//Redskins
+data['Redskins 1983-Present']=['End','NFC East','Washington Redskins','1983-Present','nfc/east/redskins1983.gif'];
+data['Redskins 1982-1982']=['End','NFC East','Washington Redskins','1982-1982','nfc/east/redskins1982.gif'];
+data['Redskins 1972-1981']=['End','NFC East','Washington Redskins','1972-1981','nfc/east/redskins1972.gif'];
+data['Redskins 1970-1971']=['End','NFC East','Washington Redskins','1970-1971','nfc/east/redskins1970.gif'];
+data['Redskins 1965-1969']=['End','NFC East','Washington Redskins','1965-1969','nfc/east/redskins1965.gif'];
+data['Redskins 1960-1964']=['End','NFC East','Washington Redskins','1960-1964','nfc/east/redskins1960.gif'];
+data['Redskins 1952-1959']=['End','NFC East','Washington Redskins','1952-1959','nfc/east/redskins1952.gif'];
+data['Redskins 1937-1951']=['End','NFC East','Washington Redskins','1937-1951','nfc/east/redskins1937.gif'];
+data['Redskins 1933-1936']=['End','NFC East','Washington Redskins','1933-1936','nfc/east/redskins1933.gif'];
---
+++ functions2.js
@@ -0,0 +1,176 @@
+
+        function loadFunction(){
+        //var getName=prompt('Hi! First time here, what is your name?','');
+
+            cookies();
+            var div = document.createElement("div");
+           // div.setAttribute("onchange","move(this)");
+          // document.getElementById("div").addEventListener("change", move);
+             div.setAttribute("id","div1");
+                        document.getElementById("form").appendChild(div);
+
+            document.getElementById("div1").addEventListener("change", function(){ move(this); });
+
+
+
+            //div.setAttribute("id","div1");
+            //document.getElementById("form").appendChild(div);
+             var choices = data['leagues'];
+            var question = document.createTextNode(choices[0]);
+            div.appendChild(question);
+            var sel1 = document.createElement("SELECT");
+            sel1.setAttribute("id","select1");
+            //sel1.setAttribute("onchange", "changeTeam(this.value)");
+            //sel1.addEventListener("change", changeTeam);
+            div.appendChild(sel1);
+            document.getElementById("select1").addEventListener("change", function(){ changeTeam(this.value) });
+
+           // div.appendChild(sel1);
+             var choices = data['leagues'];
+
+            for (i=0; i<choices.length;i++){
+                var opt = document.createElement("OPTION");
+                var ch = document.createTextNode(choices[i]);  
+                opt.appendChild(ch);
+                sel1.appendChild(opt); 
+            }
+        }
+//changeTeam()        
+        function changeTeam(choice){
+            console.log(choice);
+           // var nodelist = document.getElementsByTagName("DIV").length;
+           // console.log(nodelist);
+            var choices = data[choice];
+        
+            if(choices[0]=="End"){
+                storeValues();
+           
+                var span = document.createElement("span");
+
+                span.setAttribute("id","sum");
+                document.body.appendChild(span);
+                
+               // var statement=document.createTextNode(summary);
+                span.appendChild(document.createTextNode("Division:   "+localStorage.getItem('sel0')));//+choices[1]));
+                //span.appendChild(statement);
+               span.appendChild(document.createElement("br")); span.appendChild(document.createTextNode("Team:   "+localStorage.getItem('sel1')));//+choices[2]));
+               span.appendChild(document.createElement("br")); span.appendChild(document.createTextNode("Logo Years:   "+localStorage.getItem('sel2')));//+choices[3]));
+                span.appendChild(document.createElement("br"));
+                
+                var span2 = document.createElement("span");
+                span2.setAttribute("id","jersey");
+                span.appendChild(span2);
+                 var y = document.createElement("IMG");
+                //y.setAttribute("id","jersey")
+                 y.setAttribute("src", "jersey.jpg");
+                y.setAttribute("width", "500px")
+                span2.appendChild(y);
+                
+                var span3 = document.createElement("span");
+                span3.setAttribute("id","logo");
+                span.appendChild(span3);
+                var x = document.createElement("IMG");
+                 x.setAttribute("src", choices[4]);
+                //x.setAttribute("id","logo")
+
+                x.setAttribute("width", "200px")
+                span3.appendChild(x);
+            }
+            
+            else{
+               
+                
+			var myNode = document.getElementById(choice.parentNode.id);
+                var num=nodelist+1;
+                removeOld(myNode);
+             var div = document.createElement("div");
+            div.setAttribute("id","div"+num);
+            div.setAttribute("onchange","move(this)");
+
+            document.getElementById("form").appendChild(div);
+                          
+
+            
+            var question = document.createTextNode(choices[0] );
+            div.appendChild(question);
+            var sel1 = document.createElement("SELECT");
+           sel1.setAttribute("id","select"+num);
+            sel1.setAttribute("onchange", "changeTeam(this.value)");
+            div.appendChild(sel1);
+            document.getElementById("select"+num).addEventListener("change", function(){ changeTeam(this.value) });
+
+           
+            for (i=0; i<choices.length;i++){
+                var opt = document.createElement("OPTION");
+                var ch = document.createTextNode(choices[i]);  
+                opt.appendChild(ch);
+                sel1.appendChild(opt);  
+            }
+            }
+        }
+        
+//removeOld()        
+		function removeOld(myDiv){
+            var summary=document.getElementById("sum");
+       if (summary !=null){
+            summary.parentNode.removeChild(summary);
+
+       }
+		while (myDiv.firstChild && myDiv.nextSibling !=null)
+		myDiv.parentNode.removeChild(myDiv.nextSibling);
+        
+        }
+
+
+//move 
+    function move(ele, val)
+    	{
+      var curLeft = parseInt(window.getComputedStyle(ele).left);
+      var newLeft = curLeft + 10;
+      
+      ele.style.left = newLeft + "px";
+
+      if (newLeft <= 900)
+      {
+        setTimeout(function()
+        {
+          move(ele);
+        }, 50);        
+      }
+	else{
+	
+	}
+    }
+
+//removeAll
+    function removeAll(){
+        	var divs = document.getElementsByTagName('div');
+		var len = divs.length;
+		//debugger;
+		for( var i=0; i<len; i++){
+			document.getElementById('form').removeChild(divs[0]);
+		} // end for
+          var summary=document.getElementById("sum");
+       if (summary !=null){
+            summary.parentNode.removeChild(summary);
+
+       }
+        loadFunction();
+    }
+
+//cookies
+    function cookies(){
+        console.log("Cookies enabled is " + navigator.cookieEnabled);
+        if(navigator.cookieEnabled==false){
+            alert("Cookies Not Enabled");
+           var win=window.open( "http://filehippo.com/software/internet/browsers/");
+        }  
+    }
+function storeValues(){
+    var select = document.getElementsByTagName('select');
+    var len = select.length;
+    for(var i=1; i<len; i++){
+     localStorage.setItem('sel'+[i], select[i].value);
+    }
+    
+}
